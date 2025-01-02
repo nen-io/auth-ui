c5e49f4b11e8964d7a27e2b027af6fa51b4075b2
Ajith Abraham <ajith.abraham@phmg.com>
2025-01-02 18:44:12 +0000

    clean up files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Router } from "@solidjs/router";
-import SignIn from "./pages/signIn";
+import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
-import rootLayout from "./pages/rootLayout";
+import rootLayout from "./layout/rootLayout";
 import Home from "./pages/Home";
 
 export default () => {
   return (
     <Router root={rootLayout}>
-      <Route path={"/"} component={() => <Home />} />
+      <Route path={"/"} component={Home} />
       <Route path={"/SignIn"} component={SignIn} />
       <Route path={"/Register"} component={Register} />
     </Router>
